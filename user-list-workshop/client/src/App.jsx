4cec2e3a0335c3251c1c4db4b78f6d6e16cecee1
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-11-12 14:57:09 +0200

    create components

--- user-list-workshop/client/src/App.jsx
+++ user-list-workshop/client/src/App.jsx
@@ -1,8 +1,17 @@
-import './styles.css'
+import './styles.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import UserList from './components/UserList';
 
 function App() {
   return (
-
+    <div>
+      <Header />
+      <main className="main">
+      <UserList />
+      </main>
+      <Footer />
+    </div>
   )
 }
 
